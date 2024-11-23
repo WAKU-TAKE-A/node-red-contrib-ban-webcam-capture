846eb428e34f2265a43578f65c3ddd458486a45e
WAKU-TAKE-A <waku-take-a@ymail.ne.jp>
2024-11-24 01:03:38 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# node-red-contrib-ban-webcam-capture
+
+Node-RED node for capturing images from a webcam. 
+
+It's for practice
---
+++ webcam-capture.js
@@ -0,0 +1,35 @@
+const NodeWebcam = require("node-webcam");
+
+module.exports = function (RED) {
+  function WebcamCapture(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+
+    let webcam = null;  // 初期化時にカメラを一度だけ作成
+
+    node.on("input", function (msg) {
+      // 初回のみカメラのインスタンスを作成
+      if (!webcam) {
+        const camOptions = {
+          width: msg.width || 640,
+          height: msg.height || 480,
+          device: msg.id || "",
+          callbackReturn: "buffer"  // バイナリで取得
+        };
+        webcam = NodeWebcam.create(camOptions);
+      }
+
+      // キャプチャを実行
+      webcam.capture("temp", function (err, data) {
+        if (err) {
+          node.error("Error capturing image: " + err, msg);
+          return;
+        }
+        msg.payload = data; // バイナリデータを出力
+        node.send(msg);
+      });
+    });
+  }
+
+  RED.nodes.registerType("webcam-capture", WebcamCapture);
+};
